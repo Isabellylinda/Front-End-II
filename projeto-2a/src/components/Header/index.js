43ad0275d1859dd804facc044b5057f7e0bb7f9b
Isabellylinda <Isabelly.lbispo@gmail.com>
2024-11-14 21:52:49 -0400

    savd

--- projeto-2a/src/components/Header/index.js
+++ projeto-2a/src/components/Header/index.js
@@ -1,29 +1,30 @@
-'use client';
-
+'use client'
 import { useState } from "react";
 import Link from "next/link";
+import Image from "next/image";
 import styles from "./Header.module.css";
 
-const Header = () => {
-  const [menuAberto, setMenuAberto] = useState(false);
+export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => {
-    setMenuAberto(!menuAberto);
+    setIsOpen(!isOpen);
   };
 
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>Meu Logo</div>
-      <div className={`${styles.menuIcon} ${menuAberto ? styles.open : ''}`} onClick={toggleMenu}>
-        ☰
+      <Image src="/isa.png" alt="Logo Isa" className={styles.logoImage} width={50} height={50} />
+      <div className={styles.menuIcon} onClick={toggleMenu}>
+        <span className={isOpen ? styles.menuOpen : ""}></span>
       </div>
-      <nav className={`${styles.nav} ${menuAberto ? styles.show : ''}`}>
-        <Link href="/">Home</Link>
-        <Link href="/sobre">Sobre</Link>
-        <Link href="/contato">Contato</Link>
+      <nav className={`${styles.nav} ${isOpen ? styles.showNav : ""}`}>
+        <ul>
+          <li><Link href="/home">Home</Link></li>
+          <li><Link href="/sobre">Sobre</Link></li>
+          <li><Link href="/contato">Contato</Link></li>
+        </ul>
       </nav>
     </header>
   );
-};
+}
 
-export default Header;
