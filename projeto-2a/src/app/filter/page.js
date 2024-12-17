8539dda641a17ee206b74dac95a40c66e93110b7
Isabellylinda <Isabelly.lbispo@gmail.com>
2024-12-17 09:05:44 -0400

    save

--- projeto-2a/src/app/filter/page.js
+++ projeto-2a/src/app/filter/page.js
@@ -2,7 +2,7 @@
 import { useState } from "react";
 
 export default function Filter () {
-    const nomes = ['Wagner', 'Isabelly', 'Ana', 'theo', 'João']
+    const nomes = ['dayanny', 'Isabelly', 'Ana', 'theo', 'João']
     const nums = [1,2,3,4,5,6,7,8,9,10];
     const pares = nums.filter( num => num % 2 == 0);
     const [busca, setBusca] = useState('')
