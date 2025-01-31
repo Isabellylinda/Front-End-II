876d37daf86d6f76c2f0c0e19f0128080619099e
Isabellylinda <Isabelly.lbispo@gmail.com>
2025-01-31 08:22:32 -0400

    save

---
+++ projeto_teste/projeto_teste/src/app/medico/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+      <main>
+      <h1>Medico</h1>
+      </main>
+    );
+  }
---
+++ projeto_teste/projeto_teste/src/components/Header/Header.js
@@ -0,0 +1,44 @@
+'use client';
+import Link from "next/link";
+import styles from "./Header.module.css"
+import { useState } from "react";
+import Image from "next/image";
+
+
+
+export default function Header() {
+    const [showMenu, setShowMenu] = useState(true)
+
+    return (
+        <header>
+            <button onClick={() => setShowMenu(!showMenu)}>
+                <Image className={styles.rat}
+                    src="/menu.png"
+                    alt="Ícone do menu"
+                    width={30}
+                    height={30}
+
+                />
+            </button>
+            {
+
+                showMenu &&
+
+                <nav>
+                    <ul className={styles.li}>
+                        <img className={styles.me}
+                            src="/BD2.png"
+                            alt="menu"
+                            width={75}
+                            height={40}
+                        />
+                        <li><Link href='/'>Home</Link></li>
+                        <li><Link href='/medico'>Medico</Link></li>
+                        <li><Link href='/paciente'>Paciente</Link></li>
+                        <li><Link href='/consulta'>Consulta</Link></li>
+                    </ul>
+                </nav>
+            }
+        </header>
+    )
+} 
